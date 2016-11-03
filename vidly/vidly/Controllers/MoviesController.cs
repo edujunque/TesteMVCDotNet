d5e82cfcb54eac318143739f81a71309ada75306
Eduardo Junque <eduardo.junque@navegarti.com.br>
2016-11-03 18:05:05 -0200

    Customers and videos, Input and edit

--- vidly/vidly/Controllers/MoviesController.cs
+++ vidly/vidly/Controllers/MoviesController.cs
@@ -31,7 +31,7 @@ namespace vidly.Controllers
             var viewModel = new IndexMovieViewModel()
             {
                 Movies = movies,
-              
+
             };
             return View(viewModel);
 
@@ -65,13 +65,6 @@ namespace vidly.Controllers
 
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-       
-
         [Route("movies/released/{year}/{month}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
@@ -86,6 +79,54 @@ namespace vidly.Controllers
 
             return View(movies);
 
+        }
+
+        public ActionResult New()
+        {
+            var genre = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genre
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
         }
 
     }
